1f6779b31189cebfd5ea399c6ec6f3cea9d39143
Kevalin Ketcham <kevalin.create@gmail.com>
2020-12-10 03:28:07 -0800

    Have scraper fetch all div's on page

--- scraper.py
+++ scraper.py
@@ -1,4 +1,12 @@
 import requests
+from bs4 import BeautifulSoup
 
-URL = "https://www.twitter.com/KevalinKetcham"
+URL = 'https://www.twitter.com/KevalinKetcham'
+page = requests.get(URL)
+soup = BeautifulSoup(page.content, 'html.parser')
 
+tweets = soup.find_all("div")
+print(tweets)
+
+
+# , attrs={"class"="css-1dbjc4n"}
